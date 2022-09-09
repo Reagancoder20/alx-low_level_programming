f40bf2f4937b8b6fe8444c58083bc971d314e1ea
Reagancoder20 <rplubega@gmail.com>
2022-09-09 02:51:36 -0700

    h

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
-/**
-* main - function
-* 
-*Return - number/meaning
-*
-*
-*/
 #include <stdio.h>
+
+/**
+*main -Entry point
+*Return: 0
+*/
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-}	
+	puts("\"Programming is like building a multilingual puzzle\n");
 return(0);
-
+}
